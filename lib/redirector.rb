3b0175e41c836ecb6d249695026434a18148b507
Tonda Pleskac <pleskac@gmail.com>
2016-09-20 11:36:16 +0200

    Add blacklisted extensions options and disbaled env_variables

--- lib/redirector.rb
+++ lib/redirector.rb
@@ -5,6 +5,8 @@ module Redirector
   mattr_accessor :include_query_in_source
   mattr_accessor :preserve_query
   mattr_accessor :silence_sql_logs
+  mattr_accessor :use_environment_variables
+  mattr_accessor :blacklisted_extensions
 
   def self.active_record_protected_attributes?
     @active_record_protected_attributes ||= Rails.version.to_i < 4 || defined?(ProtectedAttributes)
